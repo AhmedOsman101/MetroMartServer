e7f0c3e7b461e6febe3c3c2c8dd0e708c53b8bce
AhmadOthman <trollingosman101@outlook.com>
2024-01-07 09:26:25 +0200

    formatting

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_USER = process.env.DB_USER;
 const Allowed_ip = process.env.ALLOWED_IP;
 
-const Allowed_ips = JSON.parse( Allowed_ip );
+const Allowed_ips = JSON.parse(Allowed_ip);
 
 const App = express(); // Create a new Express application
 App.use(express.json()); // Use express.json() middleware for parsing JSON data of incoming requests
@@ -26,130 +26,102 @@ const connection = sql.createConnection({
 	database: DB_NAME,
 });
 
-
-
-
-
 // Show all users
-App.get( "/", ( req, res ) =>
-{
-	if (Allowed_ips.indexOf(req.ip)!== -1) {
+App.get("/", (req, res) => {
+	if (Allowed_ips.indexOf(req.ip) !== -1) {
 		const query = "SELECT * FROM `users`";
-		connection.execute( query, ( err, data ) =>
-		{
-			if ( err ) res.send( `ERROR: ${ err }` );
-			else res.json( data );
-		} );
-	} else{
-		res.status( 401 ).send("authentication refused")
+		connection.execute(query, (err, data) => {
+			if (err) res.send(`ERROR: ${err}`);
+			else res.json(data);
+		});
+	} else {
+		res.status(401).send("authentication refused");
 	}
-	
 });
 
 // select specific user to show
-App.post( "/login", ( req, res ) =>
-{
-	if ( Allowed_ips.indexOf( req.ip ) !== -1 )
-	{
+App.post("/login", (req, res) => {
+	if (Allowed_ips.indexOf(req.ip) !== -1) {
 		const { email, password } = req.body;
-		const values = [ email, password ];
+		const values = [email, password];
 		let query = "SELECT email FROM `users` WHERE `email` = ?";
-		connection.execute( query, [ email ], ( err, data ) =>
-		{
-			if ( err ) res.send( `ERROR: ${ err }` );
-			else if ( 0 == data.length ) res.send( "ERROR: this mail doesn't exist" );
-			else
-			{
-				res.send( "login successfully" );
+		connection.execute(query, [email], (err, data) => {
+			if (err) res.send(`ERROR: ${err}`);
+			else if (0 == data.length)
+				res.send("ERROR: this mail doesn't exist");
+			else {
+				res.send("login successfully");
 			}
-		} );
-	} else
-	{
-		res.status( 401 ).send( "authentication refused" );
+		});
+	} else {
+		res.status(401).send("authentication refused");
 	}
-	
 });
 
 // Handle POST requests to add users
-App.post( "/User/Add", ( req, res ) =>
-{
-	if ( Allowed_ips.indexOf( req.ip ) !== -1 )
-	{
+App.post("/User/Add", (req, res) => {
+	if (Allowed_ips.indexOf(req.ip) !== -1) {
 		const { email, password } = req.body;
-		const values = [ email, password ];
+		const values = [email, password];
 		let query = "SELECT email FROM `users` WHERE `email` = ?";
-		connection.execute( query, [ email ], ( err, data ) =>
-		{
-			if ( err ) res.send( `ERROR: ${ err }` );
-			else if ( 0 < data.length ) res.send( "ERROR: this mail exists" );
-			else
-			{
+		connection.execute(query, [email], (err, data) => {
+			if (err) res.send(`ERROR: ${err}`);
+			else if (0 < data.length) res.send("ERROR: this mail exists");
+			else {
 				query =
 					"INSERT INTO `users`(`username`, `email`, `password`) VALUES (?, ?, ?)";
-				connection.execute( query, values, ( err ) =>
-				{
-					if ( err ) res.send( `ERROR: ${ err }` );
-					else
-					{
-						connection.execute( "SELECT * FROM `users`", ( err, data ) =>
-						{
-							if ( err ) res.send( `ERROR: ${ err }` );
-							else res.send( data );
-						} );
+				connection.execute(query, values, (err) => {
+					if (err) res.send(`ERROR: ${err}`);
+					else {
+						connection.execute(
+							"SELECT * FROM `users`",
+							(err, data) => {
+								if (err) res.send(`ERROR: ${err}`);
+								else res.send(data);
+							}
+						);
 					}
-				} );
+				});
 			}
-		} );
-	} else
-	{
-		res.status( 401 ).send( "authentication refused" );
+		});
+	} else {
+		res.status(401).send("authentication refused");
 	}
-	
 });
 
 // Handle DELETE requests to remove users
-App.delete( "/User/Delete/:id", ( req, res ) =>
-{
-	if ( Allowed_ips.indexOf( req.ip ) !== -1 )
-	{
+App.delete("/User/Delete/:id", (req, res) => {
+	if (Allowed_ips.indexOf(req.ip) !== -1) {
 		const userID = req.params.id;
 		const query = "DELETE FROM `users` WHERE id = ?";
-		connection.execute( query, [ userID ], ( err, data ) =>
-		{
-			if ( err ) res.send( `ERROR: ${ err }` );
-			else if ( data.affectedRows === 0 )
-				res.send( "ERROR: USER WAS NOT FOUND !!!" );
-			else res.send( "USER DELETED SUCCESSFULLY" );
-		} );
-	} else
-	{
-		res.status( 401 ).send( "authentication refused" );
+		connection.execute(query, [userID], (err, data) => {
+			if (err) res.send(`ERROR: ${err}`);
+			else if (data.affectedRows === 0)
+				res.send("ERROR: USER WAS NOT FOUND !!!");
+			else res.send("USER DELETED SUCCESSFULLY");
+		});
+	} else {
+		res.status(401).send("authentication refused");
 	}
-
 });
 
 // Handle PUT requests to update users
-App.put( "/User/Update/:id", ( req, res ) =>
-{
-	if ( Allowed_ips.indexOf( req.ip ) !== -1 )
-	{
+App.put("/User/Update/:id", (req, res) => {
+	if (Allowed_ips.indexOf(req.ip) !== -1) {
 		const userID = req.params.id;
 		const { username, email, password } = req.body;
-		const values = [ username, email, password, userID ];
+		const values = [username, email, password, userID];
 		const query =
 			"UPDATE `users` SET `username` = ?, `email` = ?, `password` = ? WHERE `id` = ?";
-		connection.execute( query, values, ( err, data ) =>
-		{
-			if ( err ) res.send( `ERROR: ${ err }` );
-			else if ( data.affectedRows === 0 )
-				res.send( "ERROR: USER WAS NOT FOUND !!!" );
-			else res.send( "USER UPDATED SUCCESSFULLY" );
-		} );
-	} else
-	{
-		res.status( 401 ).send( "authentication refused" );
+		connection.execute(query, values, (err, data) => {
+			if (err) res.send(`ERROR: ${err}`);
+			else if (data.affectedRows === 0)
+				res.send("ERROR: USER WAS NOT FOUND !!!");
+			else res.send("USER UPDATED SUCCESSFULLY");
+		});
+	} else {
+		res.status(401).send("authentication refused");
 	}
-	
 });
 
 // handle any other requests
@@ -159,5 +131,5 @@ App.use((req, res) => {
 
 // Start the server and have it listen on the specified port
 App.listen(port, () => {
-	console.log( `Server is running on http://localhost:${ port }` );
+	console.log(`Server is running on http://localhost:${port}`);
 });
