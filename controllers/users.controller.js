935facaf2c4a1134517e06892ad66f06ec230e4e
AhmadOthman <trollingosman101@outlook.com>
2024-01-12 17:40:03 +0200

    Refactor user controllers

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -10,8 +10,10 @@ const httpStatusText = require("../utils/httpStatustext");
 const User = require("../models/users");
 const bcrypt = require("bcryptjs");
 
-// function to show all users from database
-const getAllusers = async (req, res) => {
+const getAllusers = async (
+	req,
+	res // function to show all users from database
+) => {
 	if (Admins_ip.indexOf(req.ip) !== -1) {
 		try {
 			const users = await User.findAll();
@@ -34,44 +36,63 @@ const getAllusers = async (req, res) => {
 	}
 };
 
-const login = async ( req, res ) =>// login function
-{
-    if ( Allowed_ips.indexOf( req.ip ) !== -1 )
-    {
-        const { email, password } = req.body;
-        try {
-            const user = await User.findAll( {
-                where: {
-                    email: email
-                }
-            } );
-            if ( user.length > 0 )
-            {
-                console.log( user[ 0 ].password, "----", password )
-                const matchPassword = await bcrypt.compare(password,user[0].password);
-                if ( matchPassword )
-                {
-                    res.status( 200 ).send( { status: httpStatusText.SUCCESS, data: user[ 0 ] } );
-                } else
-                {
-                    res.status( 400 ).send( { status: httpStatusText.FAIL, data: null, msg: 'wrong password' } );
-                }
-            } else
-            {
-                res.status( 404 ).send( { status: httpStatusText.FAIL, data: null , msg : "email doesn't exists" }   );
-            }
-        } catch (error) {
-            res.status( 400 ).send( { status: httpStatusText.FAIL, data: null, msg: error.errors[ 0 ].message })
-        }
-        
-    } else
-    {
-        res.status( 401 ).send( { status: httpStatusText.FAIL, data: null, msg: "authentication refused" } ); 
-    }
+const login = async (
+	req,
+	res // login function
+) => {
+	if (Allowed_ips.indexOf(req.ip) !== -1) {
+		const { email, password } = req.body;
+		try {
+			const user = await User.findAll({
+				where: {
+					email: email,
+				},
+			});
+			if (user.length > 0) {
+				console.log(user[0].password, "----", password);
+				const matchPassword = await bcrypt.compare(
+					password,
+					user[0].password
+				);
+				if (matchPassword) {
+					res.status(200).send({
+						status: httpStatusText.SUCCESS,
+						data: user[0],
+					});
+				} else {
+					res.status(400).send({
+						status: httpStatusText.FAIL,
+						data: null,
+						msg: "wrong password",
+					});
+				}
+			} else {
+				res.status(404).send({
+					status: httpStatusText.FAIL,
+					data: null,
+					msg: "email doesn't exists",
+				});
+			}
+		} catch (error) {
+			res.status(400).send({
+				status: httpStatusText.FAIL,
+				data: null,
+				msg: error.errors[0].message,
+			});
+		}
+	} else {
+		res.status(401).send({
+			status: httpStatusText.FAIL,
+			data: null,
+			msg: "authentication refused",
+		});
+	}
 };
 
-//signup function
-const signup = async (req, res) => {
+const signup = async (
+	req,
+	res //signup function
+) => {
 	if (Allowed_ips.indexOf(req.ip) !== -1) {
 		const errors = validationResult(req);
 		if (errors.isEmpty()) {
@@ -111,7 +132,7 @@ const signup = async (req, res) => {
 			}
 		} else {
 			res.status(400).send({
-				status: httpStatusText.FAIL,
+				status: httpStatusText.Fail,
 				data: null,
 				msg: errors.array()[0]["msg"],
 			});
@@ -127,19 +148,25 @@ const signup = async (req, res) => {
 	}
 };
 
-// delete function
-const deleteAccount = async (req, res) => {
+const deleteAccount = async (
+	req,
+	res // delete function
+) => {
 	if (Allowed_ips.indexOf(req.ip) !== -1) {
 		const { email, password } = req.body;
 		try {
-			let user = await User.findAll({
+			user = await User.findAll({
 				where: {
 					email: email,
 				},
 			});
 
 			if (user.length > 0) {
-				if (user[0].password == password) {
+				const matchPassword = await bcrypt.compare(
+					password,
+					user[0].password
+				);
+				if (matchPassword) {
 					try {
 						await User.destroy({
 							where: {
@@ -188,101 +215,94 @@ const deleteAccount = async (req, res) => {
 	}
 };
 
-            if ( user.length > 0 )
-            {
-                const matchPassword = await bcrypt.compare( password, user[ 0 ].password );
-                if ( matchPassword )
-                {
-                    try {
-                        await User.destroy( {
-                            where: {
-                                email: email
-                            }
-                        } );
-                        res.status( 200 ).send( { status: httpStatusText.SUCCESS, data: null, msg: "user deleted" } )
-                    }catch (error) {
-                        res.status( 400 ).send( { status: httpStatusText.FAIL, data: null, msg: error.errors[ 0 ].message } )
-                    }
-                    res.status( 200 ).send( { status: httpStatusText.SUCCESS, data: user[ 0 ] } );
-                } else
-                {
-                    res.status( 400 ).send( { status: httpStatusText.FAIL, data: null, msg: 'wrong password' } );
-                }
-            } else
-            {
-                res.status( 404 ).send( { status: httpStatusText.FAIL, data: null, msg: "email doesn't exists" } );
-            }
-        } catch ( error )
-        {
-            res.status( 400 ).send( { status: httpStatusText.FAIL, data: null, msg: error } );
-        }
-    } else
-    {
-        res.status( 401 ).send( "authentication refused" );
-    }
-}
-
-
-const updateAccount = async( req, res ) =>
-{
-    if ( Allowed_ips.indexOf( req.ip ) !== -1 )
-    {
-        const { name, email, password,new_password, address1, address2, phone_number,gender,age } = req.body
-        const errors = validationResult( req );
-        if ( errors.isEmpty() )
-        {
-            try
-            {
-                user = await User.findAll( {
-                    where: {
-                        email: email
-                    }
-                } );
-                if ( user.length > 0 )
-                {
-                    const matchPassword = await bcrypt.compare( password, user[ 0 ].password );
-                    if ( matchPassword )
-                    {
-                        try {
-                            await User.update( {
-                                "name": name,
-                                "email": email,
-                                "password": new_password,
-                                "address1": address1,
-                                "address2": address2,
-                                "phone_number": phone_number,
-                                "gender": gender,
-                                "age": age
-                            }, {
-                                where: {
-                                    'email': email
-                                }
-                            } );
-                            res.status( 200 ).send( { status: httpStatusText.SUCCESS, data: null , msg : "user has been updated successfully" } )
-                        } catch (error) {
-                            res.status(400).send(error)
-                        }
-                        
-                    } else
-                    {
-                        res.status( 400 ).send( { status: httpStatusText.FAIL, data: null, msg: 'wrong password' } );
-                    }
-                } else
-                {
-                    res.status( 404 ).send( { status: httpStatusText.FAIL, data: null, msg: "email doesn't exists" } );
-                }
-            } catch ( error )
-            {
-                res.status( 400 ).send( { status: httpStatusText.FAIL, data: null, msg: error.errors[ 0 ].message } );
-            }
-        } else
-        {
-            res.status( 400 ).send( { status: httpStatusText.FAIL, data: null, msg:errors.array()[ 0 ][ 'msg' ] });
-        }
-    } else
-    {
-        res.status( 401 ).send( { status: httpStatusText.FAIL, data: null, msg:"authentication refused"  });
-    }
+const updateAccount = async (req, res) => {
+	if (Allowed_ips.indexOf(req.ip) !== -1) {
+		const {
+			name,
+			email,
+			password,
+			new_password,
+			address1,
+			address2,
+			phone_number,
+			gender,
+			age,
+		} = req.body;
+		const errors = validationResult(req);
+		if (errors.isEmpty()) {
+			try {
+				user = await User.findAll({
+					where: {
+						email: email,
+					},
+				});
+				if (user.length > 0) {
+					const matchPassword = await bcrypt.compare(
+						password,
+						user[0].password
+					);
+					if (matchPassword) {
+						try {
+							await User.update(
+								{
+									name: name,
+									email: email,
+									password: new_password,
+									address1: address1,
+									address2: address2,
+									phone_number: phone_number,
+									gender: gender,
+									age: age,
+								},
+								{
+									where: {
+										email: email,
+									},
+								}
+							);
+							res.status(200).send({
+								status: httpStatusText.SUCCESS,
+								data: null,
+								msg: "user has been updated successfully",
+							});
+						} catch (error) {
+							res.status(400).send(error);
+						}
+					} else {
+						res.status(400).send({
+							status: httpStatusText.FAIL,
+							data: null,
+							msg: "wrong password",
+						});
+					}
+				} else {
+					res.status(404).send({
+						status: httpStatusText.FAIL,
+						data: null,
+						msg: "email doesn't exists",
+					});
+				}
+			} catch (error) {
+				res.status(400).send({
+					status: httpStatusText.FAIL,
+					data: null,
+					msg: error.errors[0].message,
+				});
+			}
+		} else {
+			res.status(400).send({
+				status: httpStatusText.FAIL,
+				data: null,
+				msg: errors.array()[0]["msg"],
+			});
+		}
+	} else {
+		res.status(401).send({
+			status: httpStatusText.FAIL,
+			data: null,
+			msg: "authentication refused",
+		});
+	}
 };
 
 module.exports = {
