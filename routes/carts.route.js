3358ca7d0613c69044046f68f432cb3284267f7d
YassenMohamed <yassenmohamedrashad@gmail.com>
2024-01-13 20:12:17 +0200

    .

--- routes/carts.route.js
+++ routes/carts.route.js
@@ -2,14 +2,15 @@ const cartsController = require( "../controllers/carts.controller" );
 // const { body, validationResult } = require( 'express-validator' );
 const express = require( 'express' );
 const cartRoute = express.Router();
+const verifyToken = require( "../middleware/verfiyToken" )
 
 
 
 
 
 cartRoute.get( "/",cartsController.getAllCarts);
-cartRoute.get( "/getSingleCart", cartsController.getSingleCart );
-cartRoute.post( "/addProductToCart", cartsController.addProductToCart );
+cartRoute.get( "/getSingleCart",verifyToken, cartsController.getSingleCart );
+cartRoute.post( "/addProductToCart",verifyToken, cartsController.addProductToCart );
 
 
 
